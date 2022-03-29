782ce185e4a21041dc38ca2500ed6a718a27ba43
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-03-29 22:58:14 +0100

    creation of the bottom navigation bar

--- app/src/main/java/com/example/sihati_client/mainPage/MainActivity.kt
+++ app/src/main/java/com/example/sihati_client/mainPage/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.sihati_client.mainPage
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.sihati_client.R
 import com.example.sihati_client.mainPage.fragments.HealthStatusFragment
 import com.example.sihati_client.mainPage.fragments.SchedulesFragment
 import com.example.sihati_client.mainPage.fragments.TestHistoryFragment
-import com.google.android.material.navigation.NavigationBarView
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
@@ -26,17 +23,17 @@ class MainActivity : AppCompatActivity() {
         //set a default value for the fragment
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment, HealthStatusFragment()).commit()
-        bottomNavigationView.setItemSelected(R.id.healthStatusFragment)
+        bottomNavigationView.setItemSelected(R.id.healthStatus)
         //display the selected fragment using setOnItemSelectedListener function
         bottomNavigationView.setOnItemSelectedListener{
             when (it) {
-                R.id.healthStatusFragment -> {
+                R.id.healthStatus -> {
                     fragment = HealthStatusFragment()
                 }
-                R.id.testHistoryFragment -> {
+                R.id.testHistory -> {
                     fragment = TestHistoryFragment()
                 }
-                R.id.schedulesFragment -> {
+                R.id.schedules -> {
                     fragment = SchedulesFragment()
                 }
             }
