681474e23aba6e06e0adaca2bdddd8f361d59d03
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-04-14 22:34:45 +0100

    setup the test files : TestViewModel.kt TestRepository.kt Test.kt TestAdapter.kt

---
+++ app/src/main/java/com/example/sihati_client/database/Test.kt
@@ -0,0 +1,8 @@
+package com.example.sihati_client.database
+
+data class Test (
+    var laboratory_id:String? = "",
+    var result:String? = "",
+    var user_id:String? = "",
+    var schedule_id:String? = ""
+)
