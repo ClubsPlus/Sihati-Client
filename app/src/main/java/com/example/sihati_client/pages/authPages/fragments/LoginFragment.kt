b21ba758013971a5700b737d63aa0ba2a4611824
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-05-11 23:37:02 +0100

    start created password recovery

--- app/src/main/java/com/example/sihati_client/pages/authPages/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/sihati_client/pages/authPages/fragments/LoginFragment.kt
@@ -1,22 +1,32 @@
 package com.example.sihati_client.pages.authPages.fragments
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.sihati_client.R
 import com.example.sihati_client.databinding.FragmentLoginBinding
 import com.example.sihati_client.pages.mainPage.MainActivity
 import com.example.sihati_client.viewModels.AuthViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private lateinit var viewModel: AuthViewModel
+    private lateinit var progressDialog: ProgressDialog
+    private var email=""
+    private var firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +51,10 @@ class LoginFragment : Fragment() {
             }
         }
 
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog.setTitle("Please wait...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
 //        binding.forgetPassword.setOnClickListener {
 //            startActivity(Intent(requireActivity(), SignUpActivity::class.java))
 //        }
@@ -52,5 +66,45 @@ class LoginFragment : Fragment() {
                     binding.password.text.toString(),requireActivity())
             }else Toast.makeText(requireActivity(),"please fill your email and password", Toast.LENGTH_LONG).show()
         }
+
+        binding.forgetPassword.setOnClickListener {
+            setupEmailDialog()
+        }
+    }
+
+    private fun setupEmailDialog(){
+        val dialog = BottomSheetDialog(requireContext(),R.style.BottomSheetDialogTheme)
+        val view = layoutInflater.inflate(R.layout.email_bottom_sheet_content, null)
+        view.findViewById<Button>(R.id.send).setOnClickListener {
+            email = view.findViewById<TextInputEditText>(R.id.email).text.toString().trim()
+
+            if(email.isEmpty()){
+                Toast.makeText(requireContext(),"Enter email...",Toast.LENGTH_LONG).show()
+            }
+            else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(requireContext(),"Invalid email pattern...",Toast.LENGTH_LONG).show()
+            }
+            else{
+                recoverPassword(dialog)
+            }
+        }
+        dialog.setContentView(view)
+        dialog.show()
+    }
+
+    private fun recoverPassword(dialog: BottomSheetDialog) {
+        progressDialog.setMessage("Sending password reset instruction to $email")
+        progressDialog.show()
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(requireContext(),"Instructions send to \n$email",Toast.LENGTH_LONG).show()
+                dialog.dismiss()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(requireContext(),"Failed to send due to ${e.message}",Toast.LENGTH_LONG).show()
+            }
+
     }
 }
